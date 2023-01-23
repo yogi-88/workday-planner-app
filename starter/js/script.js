52695f4294bc4992620b26cd5d10f1c55b60394d
Yogita Negi <ynegi6@gmail.com>
2023-01-23 15:19:05 +0000

    variable timeblocks created to target each timeblock and thisHour variable created to show current hour

--- starter/js/script.js
+++ starter/js/script.js
@@ -2,5 +2,16 @@
 //Use moment api and format today date
 //Add it to the 'currentDay' element
 
+
 var todayDate = moment();
 $("#currentDay").text(todayDate.format('dddd, MMMM Do'));
+
+//2. Color code each block based on the current time
+//Create variables to target each time block
+
+var timeblocks = $('.timeblock');
+// In the html add the data-hour which represent which hour each element is 
+//Create variable for moment().format('H')(0-23) e.g thisHour
+
+var thisHour = moment().format('H');
+//console.log(thisHour)
